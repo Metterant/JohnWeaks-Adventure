cdc6c851b6e36d46cdd41821949ecd5d8d53a694
Metterant <metterantdev@gmail.com>
2025-05-06 01:35:24 +0700

    An interface used for implement update and start methods

---
+++ src/util/GameComponent.java
@@ -0,0 +1,9 @@
+package util;
+
+public interface GameComponent {
+    /** Start method used for handling initialization */
+    public void start();
+    
+    /** Update method used for repeating tasks every frame */
+    public void update();
+}
