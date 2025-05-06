e261d16ec2388a2d2cda7ddbdcc7f7ac344d6f20
Metterant <metterantdev@gmail.com>
2025-05-06 21:56:45 +0700

    Added methods for providing time

--- src/main/GameManager.java
+++ src/main/GameManager.java
@@ -6,9 +6,40 @@ import util.GameConstants;
 public class GameManager implements GameComponent {
 
     private int currentRound = 1; // Default round
-    private int roundDuration = GameConstants.Game.BASE_ROUND_DURATION;
 
-    // SINGLETON
+    private int roundDuration;
+    private int roundTimer = GameConstants.Game.BASE_ROUND_DURATION;
+
+    
+    public int getRoundTimer() {
+        return roundTimer;
+    }
+    
+    public int getRoundDuration() {
+        return roundDuration;
+    }
+
+    /* UTILS */
+
+    public void nextRound() {
+        currentRound++; 
+        roundTimer = GameConstants.Game.BASE_ROUND_DURATION * (currentRound - 1) * GameConstants.Game.INCREMENT_DURATION;  
+    }
+
+    //#region GameComponent
+    @Override
+    public void start() {
+        currentRound = 1;
+        roundTimer = roundDuration = GameConstants.Game.BASE_ROUND_DURATION;
+    }
+    
+    @Override
+    public void update() {
+        roundTimer--;
+    }
+    //#endregion
+
+    //#region SINGLETON
     private static GameManager instance = new GameManager();
     // Return the instance of GameManager
     public static GameManager getInstance() {
@@ -17,18 +48,5 @@ public class GameManager implements GameComponent {
 
     // Hide constructor of the singleton Class
     private GameManager() { }
-
-    public void nextRound() {
-        currentRound++; 
-        roundDuration = GameConstants.Game.BASE_ROUND_DURATION * (currentRound - 1) * GameConstants.Game.INCREMENT_DURATION;  
-    }
-
-    @Override
-    public void start() {
-        currentRound = 1;
-        roundDuration = GameConstants.Game.BASE_ROUND_DURATION;
-    }
-
-    @Override
-    public void update() { }
+    //#endregion
 }
