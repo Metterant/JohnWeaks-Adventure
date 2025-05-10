4f9febd2bbe590a3db491468dd0a5dc41194da88
Metterant <metterantdev@gmail.com>
2025-05-10 21:19:21 +0700

    Added Biker Class

---
+++ src/entity/enemy/Biker.java
@@ -0,0 +1,45 @@
+package entity.enemy;
+
+import java.awt.Graphics2D;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import entity.Enemy;
+
+public class Biker extends Enemy {
+  
+    //#region CONSTRUCTORS
+    public Biker() {
+        super();
+    }
+    public Biker(int row, int col) {
+        super(row, col);
+    }
+    public Biker(double positionX, double positionY) {
+        super(positionX, positionY);
+    }
+
+    @Override
+    public void loadImages() {
+        try {
+            sprites[0] = ImageIO.read(getClass().getResourceAsStream("/resources/entities/biker/biker1.png"));
+            sprites[1] = ImageIO.read(getClass().getResourceAsStream("/resources/entities/biker/biker2.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void dispose() {
+        throw new UnsupportedOperationException("Unimplemented method 'dispose'");
+    }
+    
+    @Override
+    public void start() {
+        health = 2; 
+        spriteCount = 2;
+
+        super.start();
+    }
+}
