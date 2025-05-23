6dd7b34affe43ace98af3e3ba11240418e6025ce
Metterant <metterantdev@gmail.com>
2025-05-23 23:45:29 +0700

    added a description for the interface

--- src/util/GameComponent.java
+++ src/util/GameComponent.java
@@ -1,5 +1,9 @@
 package util;
 
+/**
+ * Interface for game components that require initialization and per-frame updates.
+ * Implementing classes should define logic for start-up (start) and frame updates (update).
+ */
 public interface GameComponent {
     /** Start method used for handling initialization */
     public void start();
