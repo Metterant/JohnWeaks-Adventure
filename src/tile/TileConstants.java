323d5fc531c02799face87a58938126d202ac78a
Metterant <metterantdev@gmail.com>
2025-05-27 00:53:29 +0700

    Add NEXT_ROUND_TILE to TileConstants and update resource and collidable mappings

--- src/tile/TileConstants.java
+++ src/tile/TileConstants.java
@@ -3,6 +3,8 @@ package tile;
 import java.util.HashMap;
 import java.util.Map;
 
+import util.pathfinding.TileCoords;
+
 public class TileConstants {
     // TILE NAMES
     public static final int BLACK_TILE   = 0;
@@ -10,6 +12,7 @@ public class TileConstants {
     public static final int GRASS        = 2;
     public static final int WATER        = 3;
     public static final int TRAFFIC_CONE = 4;
+    public static final int NEXT_ROUND_TILE = 5;
 
     protected static final Map<Integer, String> TILE_RESOURCES_MAP = new HashMap<>();
     protected static final Map<Integer, Boolean> TILE_COLLIDABLE_MAP = new HashMap<>();
@@ -28,6 +31,7 @@ public class TileConstants {
         TILE_RESOURCES_MAP.put(GRASS,        "/resources/tiles/grass.png");
         TILE_RESOURCES_MAP.put(WATER,        "/resources/tiles/water.png");
         TILE_RESOURCES_MAP.put(TRAFFIC_CONE, "/resources/tiles/traffic_cone.png");
+        TILE_RESOURCES_MAP.put(NEXT_ROUND_TILE, "/resources/tiles/black_tile.png");
     }
 
     /** Sets each Tile Collidable property */
@@ -37,5 +41,6 @@ public class TileConstants {
         TILE_COLLIDABLE_MAP.put(GRASS,        false);
         TILE_COLLIDABLE_MAP.put(WATER,        false);
         TILE_COLLIDABLE_MAP.put(TRAFFIC_CONE, true);
+        TILE_COLLIDABLE_MAP.put(NEXT_ROUND_TILE, true);
     }
 }
